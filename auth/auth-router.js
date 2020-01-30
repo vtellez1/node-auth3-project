494c6acbb2156df8704507b653226b10ff458395
Vanessa <vanessatellez45@gmail.com>
2020-01-29 19:01:50 -0500

    working on stretch

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -44,7 +44,8 @@ router.post('/login', (req, res) => {
 function signToken(user){
     const payload = {
         userId: user.id,
-        username: user.username
+        username: user.username,
+        department: user.department
     };
 
     const options = {
